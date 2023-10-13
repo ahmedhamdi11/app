74fccf4b9d6965250320c27f8615cb406d9216ef
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-10-13 14:13:51 +0300

    firebase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,14 @@ import 'package:app/observer.dart';
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   Bloc.observer = MyBlocObserver(); // my bloc observer
   runApp(const MyApp());
 }
