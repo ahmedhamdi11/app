f4d3acde297d5267f7f96434600675362b9eee15
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-11-11 18:24:43 +0200

    navigate to ai models view

--- lib/Features/test/presentation/views/test_view.dart
+++ lib/Features/test/presentation/views/test_view.dart
@@ -1,6 +1,8 @@
 import 'package:app/Features/test/presentation/widgets/test_models_card.dart';
+import 'package:app/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:go_router/go_router.dart';
 
 class TestView extends StatelessWidget {
   const TestView({super.key});
@@ -19,28 +21,36 @@ class TestView extends StatelessWidget {
             bodyText:
                 'Test our frauds detection ai model and protect your self from credit card frauds.',
             iconPath: 'assets/icons/credit-card_icon.svg',
-            onBtnPressed: () {},
+            onBtnPressed: () {
+              GoRouter.of(context).push(AppRouter.fraudsDetectionViewPath);
+            },
           ),
           TestAiModelsCard(
             titleText: 'Attack Detection',
             bodyText:
                 'Test our Attack detection ai model and protect your self from cyber security attacks.',
             iconPath: 'assets/icons/attack_icon.svg',
-            onBtnPressed: () {},
+            onBtnPressed: () {
+              GoRouter.of(context).push(AppRouter.attackDetectionViewPath);
+            },
           ),
           TestAiModelsCard(
             titleText: 'Malware Detection',
             bodyText:
                 'Scan your device and protect your self from malware files and applications.',
             iconPath: 'assets/icons/malware_icon.svg',
-            onBtnPressed: () {},
+            onBtnPressed: () {
+              GoRouter.of(context).push(AppRouter.malwareDetectionViewPath);
+            },
           ),
           TestAiModelsCard(
             titleText: 'Phishing Emails',
             bodyText:
                 'Test our Phishing Emails detection ai model and protect your self from phishing emails.',
             iconPath: 'assets/icons/email-detection_icon.svg',
-            onBtnPressed: () {},
+            onBtnPressed: () {
+              GoRouter.of(context).push(AppRouter.emailDetectionViewPath);
+            },
           ),
           const SizedBox(
             height: 120,
