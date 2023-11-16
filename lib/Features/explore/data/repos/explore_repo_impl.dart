41257a09ef0a147f0a008a2bd043282f6f3e53b4
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-11-16 22:14:44 +0200

    implement getCyberNews method

---
+++ lib/Features/explore/data/repos/explore_repo_impl.dart
@@ -0,0 +1,40 @@
+import 'package:app/Features/explore/data/models/news_model.dart';
+import 'package:app/Features/explore/data/repos/explore_repo.dart';
+import 'package:app/core/errors/failures.dart';
+import 'package:app/core/services/api_services.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+class ExploreRepoImpl implements ExploreRepo {
+  final ApiServices apiServices;
+  ExploreRepoImpl(this.apiServices);
+
+  // get cyber news implementation
+  @override
+  Future<Either<Failure, List<NewsModel>>> getCyberNews() async {
+    const apiKey = 'd6915a64ebda4592aba0fd80f2cd88dd';
+    try {
+      // send get request
+      Response response = await apiServices.get(
+        endPoint: '/v2/everything?q=cyber security',
+        headers: {
+          'X-Api-Key': apiKey,
+        },
+      );
+
+      // add the response data to my news list
+      List<NewsModel> cyberNews = [];
+      for (int i = 0; i < response.data['articles']; i++) {
+        cyberNews.add(NewsModel.fromJson(response.data['articles'][i]));
+      }
+
+      return right(cyberNews);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDio(e));
+      } else {
+        return left(ServerFailure(e.toString()));
+      }
+    }
+  }
+}
