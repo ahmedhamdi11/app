d2f82515ff81f89ad4a4a81835dce4dd8e350364
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-11-16 21:23:00 +0200

    news api base url added

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 const kBaseUrl = 'https://glory-team.azurewebsites.net';
+const kNewsBaseUrl = 'https://newsapi.org/v2/everything?';
 
 // end points
 const kPhishingEmailEndPoint = '/phishing_email_detection';
@@ -13,7 +14,6 @@ const kPrimaryColor = Color(0xff43ED9D);
 const kSecondaryColor = Color(0xffEE7306);
 const kBackgroundColor = Color(0xff160D52);
 const kCardColor = Color(0xff241968);
-const kCardColor2 = Color(0xff2D9AE2);
 const kRedColor = Color(0xffFE4C78);
 const kWhiteColor = Colors.white;
 const kIconsBackgroundColor = Color(0xff6D50F7);
