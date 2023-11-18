c9b3bef22e7a12d873ec074210408d31af6f6175
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-11-18 19:52:18 +0200

    formate cyber news date

--- lib/Features/explore/data/models/news_model.dart
+++ lib/Features/explore/data/models/news_model.dart
@@ -1,3 +1,5 @@
+import 'package:intl/intl.dart';
+
 class NewsModel {
   final String title;
   final String description;
@@ -14,12 +16,22 @@ class NewsModel {
   });
 
   factory NewsModel.fromJson(Map<String, dynamic> jsonData) {
+    String publishedAt = '';
+
+    // formate date
+    if (jsonData['publishedAt'] != null) {
+      DateTime publishedAtDateTime = DateTime.parse(jsonData['publishedAt']);
+      publishedAt =
+          DateFormat('MMM d, At h:mm a').format(publishedAtDateTime).toString();
+    }
+
+    // return the model data
     return NewsModel(
       title: jsonData['title'] ?? '',
       description: jsonData['description'] ?? 'No description.',
       url: jsonData['url'] ?? '',
       urlToImage: jsonData['urlToImage'] ?? '',
-      publishedAt: jsonData['publishedAt'],
+      publishedAt: publishedAt,
     );
   }
 }
