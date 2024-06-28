1c9360c8d617870fba58b2610a00e5a2a78c0bb1
Ahmed Hamdi <ahmedhamdi725@gmail.com>
2024-06-28 19:05:37 +0300

    notifications services class created

---
+++ lib/core/services/notifications_service.dart
@@ -0,0 +1,70 @@
+import 'package:app/core/constants/constants.dart';
+import 'package:awesome_notifications/awesome_notifications.dart';
+
+class NotificationServices {
+  static final _awesomeNotifications = AwesomeNotifications();
+
+  static String reminderChannelKey = 'reminder_channel';
+  static String basicChannelKey = 'basic_channel';
+
+  static Future<void> initializeNotification() async {
+    await _awesomeNotifications.initialize(
+      null,
+      [
+        NotificationChannel(
+          channelKey: basicChannelKey,
+          channelName: 'Basic Notifications',
+          channelDescription: 'Basic Notification channel',
+          importance: NotificationImportance.Max,
+          channelShowBadge: true,
+          playSound: true,
+          enableVibration: true,
+          enableLights: true,
+          defaultColor: kRedColor,
+        ),
+      ],
+    );
+  }
+
+  static Future<void> showNotification({
+    required final String title,
+    required final String body,
+    required final String channelKey,
+    final ActionType actionType = ActionType.Default,
+    final NotificationLayout notificationLayout = NotificationLayout.Default,
+    final String? bigPicture,
+    final List<NotificationActionButton>? actionButtons,
+    final bool scheduled = false,
+    final int? scheduledMinute,
+    final int? scheduledHour,
+    final NotificationCategory? category,
+    final bool locked = false,
+  }) async {
+    await _awesomeNotifications.createNotification(
+      content: NotificationContent(
+        id: -1,
+        channelKey: channelKey,
+        title: title,
+        body: body,
+        actionType: actionType,
+        notificationLayout: notificationLayout,
+        bigPicture: bigPicture,
+        category: category,
+        color: kPrimaryColor,
+        locked: locked,
+      ),
+      actionButtons: actionButtons,
+      schedule: scheduled
+          ? NotificationCalendar(
+              repeats: true,
+              allowWhileIdle: true,
+              preciseAlarm: true,
+              second: 0,
+              millisecond: 0,
+              minute: scheduledMinute,
+              hour: scheduledHour,
+            )
+          : null,
+    );
+  }
+}
